6a17c8bdf82811ea80332b7d2e57ea014cd74af5
Sasin Siriskaowkul <privatebuddy@gmail.com>
2019-06-14 11:25:29 +0700

    1. Create Core App 2. Create User Model 3. Test Create User, Super User 4. Fix Flake 8

---
+++ app/core/tests.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Create User Successful"""
+        email = 'test@gmail.com'
+        password = 'test12345'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@CAPITAL.COM"
+        user = get_user_model().objects.create_user(
+            email, 'test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        """Test Create Super User"""
+        user = get_user_model().objects.create_super_user(
+            'test@testmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
